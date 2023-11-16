eff52f9cd5a47bb5b7a021f86a8a4ac210ab4a02
Kaviraja SH <shkaviraja@gmail.com>
2023-11-17 00:17:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ... (unchanged) ...
+
+function openLinkedIn() {
+    window.open("https://www.linkedin.com/in/kaviraja-sh-a040a724a/", "_blank");
+}
+
+function openGitHub() {
+    window.open("https://github.com/shkavi", "_blank");
+}
